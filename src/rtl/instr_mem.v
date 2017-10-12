24bd3c072deb589b147ea2607a6b29ff45fbe0f4
Fraser Cormack <frasercrmck@gmail.com>
2017-10-12 19:53:42 +0100

    Switch example to use p5

--- src/rtl/instr_mem.v
+++ src/rtl/instr_mem.v
@@ -25,10 +25,10 @@ initial begin
     mem[2] = {`MPY, 2'b10, 4'd0, 5'd1, 6'd0, 5'd1, 5'd2};
             // ADD R0, #1, R0
     mem[3] = {`ADD, 2'b10, 4'd0, 5'd0, 6'd0, 5'd0, 5'd1};
-            // CMP R0 {LT} #6, P0
-    mem[4] = {`CMP, 2'b10, 4'd0, 5'd0, 3'd0, `LT, 5'd0, 5'd6};
-            // BR  P0, #LOOP (-3)
-    mem[5] = {`BR,  8'd0, 3'd0, 16'hFFFD};
+            // CMP R0 {LT} #6, P5
+    mem[4] = {`CMP, 2'b10, 4'd0, 5'd5, 3'd0, `LT, 5'd0, 5'd6};
+            // BR  P5, #LOOP (-3)
+    mem[5] = {`BR,  8'd0, 3'd5, 16'hFFFD};
 
             // HALT
     mem[6] = {`HALT,  27'd0};
