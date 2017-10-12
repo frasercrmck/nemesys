460ec0c48da2961400ff96cf7fa44161d0add088
Fraser Cormack <frasercrmck@gmail.com>
2017-10-12 18:07:04 +0100

    Simplify explicit wire variables in CPU

--- src/rtl/cpu.v
+++ src/rtl/cpu.v
@@ -16,32 +16,13 @@ end
 
 wire pc_cntrl_enable = state == `WRITE_BACK;
 
-wire [(`WIDTH - 1):0] reg_data_a;
-wire [(`WIDTH - 1):0] reg_data_b;
-wire [(`WIDTH - 1):0] reg_data_z = alu_data_z;
-
 //==------------------------------------------------------------------------==//
 // Decoding logic
 //==------------------------------------------------------------------------==//
 
-wire [4:0] opcode;
-wire [(`WIDTH - 1):0] inst;
+wire take_branch = d.is_branch && regs.data_a == 1'b1;
 
-wire is_branch, halted;
-wire a_regbank_sel, b_regbank_sel, z_regbank_sel;
-
-wire take_branch = is_branch && reg_data_a == 1'b1;
-
-wire write_enable = state == `WRITE_BACK && !is_branch;
-
-wire a_from_regbank, b_from_regbank;
-wire [(`WIDTH - 1):0] a_data, b_data;
-
-wire [(`WIDTH - 1):0] alu_data_z;
-wire [(`REG_SEL - 1):0] addr_a, addr_b, addr_z;
-
-wire [2:0] cc;
-wire [31:0] pc;
+wire write_enable = state == `WRITE_BACK && !d.is_branch;
 
 //==------------------------------------------------------------------------==//
 // Unit Instantiations
@@ -50,27 +31,13 @@ wire [31:0] pc;
 instr_mem i
 (
   .clk  (clk),
-  .pc   (pc),
-  .inst (inst)
+  .pc   (pc_cntrl.pc_out)
 );
 
 decoder d
 (
-  .inst           (inst),
-  .opcode         (opcode),
-  .is_branch      (is_branch),
-  .halted         (halted),
-  .cc             (cc),
-  .a_regbank_sel  (a_regbank_sel),
-  .a_from_regbank (a_from_regbank),
-  .a_data         (a_data),
-  .a_regbank_addr (addr_a),
-  .b_regbank_sel  (b_regbank_sel),
-  .b_from_regbank (b_from_regbank),
-  .b_data         (b_data),
-  .b_regbank_addr (addr_b),
-  .z_regbank_sel  (z_regbank_sel),
-  .z_regbank_addr (addr_z)
+  .inst           (i.inst),
+  .is_branch      (is_branch)
 );
 
 pc_cntrl pc_cntrl
@@ -80,17 +47,15 @@ pc_cntrl pc_cntrl
   .enable             (pc_cntrl_enable),
   .take_branch        (take_branch),
   .is_relative_branch (take_branch), // TODO: Absolute branches
-  .branch_addr        (a_data), // Branch addresses are taken from instruction
-  .pc_out             (pc)
+  .branch_addr        (d.a_data) // Branch addresses are taken from instruction
 );
 
 alu a
 (
-  .opcode (opcode),
-  .cc     (cc),
-  .data_a (a_from_regbank ? reg_data_a : a_data),
-  .data_b (b_from_regbank ? reg_data_b : b_data),
-  .data_z (alu_data_z)
+  .opcode (d.opcode),
+  .cc     (d.cc),
+  .data_a (d.a_from_regbank ? regs.data_a : d.a_data),
+  .data_b (d.b_from_regbank ? regs.data_b : d.b_data)
 );
 
 regbank regs
@@ -98,19 +63,17 @@ regbank regs
   .clk           (clk),
   .reset         (reset),
   .write_enable  (write_enable),
-  .addr_a        (addr_a),
-  .addr_b        (addr_b),
-  .addr_z        (addr_z),
-  .data_a        (reg_data_a),
-  .data_b        (reg_data_b),
-  .data_z        (reg_data_z),
-  .a_regbank_sel (a_regbank_sel),
-  .b_regbank_sel (b_regbank_sel),
-  .z_regbank_sel (z_regbank_sel)
+  .addr_a        (d.a_regbank_addr),
+  .addr_b        (d.b_regbank_addr),
+  .addr_z        (d.z_regbank_addr),
+  .data_z        (a.data_z),
+  .a_regbank_sel (d.a_regbank_sel),
+  .b_regbank_sel (d.b_regbank_sel),
+  .z_regbank_sel (d.z_regbank_sel)
 );
 
 always @(posedge clk) begin
-  if (!halted) begin
+  if (!d.halted) begin
     state <= next_state;
     next_state <= next_state + 1;
   end
